c334e39f2ca5e583c28deb4cca27f3a0816ed149
AdrianaSchmit <schmit.adriana@gmail.com>
2021-02-28 13:24:51 +0000

    match cards function added

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let cardWasTurned = false;
+let firstCard, secondCard;
 
-function flipCard() {
-  this.classList.toggle("flip");
+function turnCard() {
+  this.classList.toggle("turn");
+
+  if (!cardWasTurned) {
+    cardWasTurned = true;
+    firstCard = this;
+      } else  {
+          cardWasTurned = false;
+    secondCard = this; 
+ 
+
+      if (firstCard.dataset.framework === secondCard.dataset.framework) {
+     firstCard.removeEventListener('click', turnCard);
+  secondCard.removeEventListener('click', turnCard);
+      }else{
+          setTimeout(() => {
+    firstCard.classList.remove('turn');
+    secondCard.classList.remove('turn');
+
+    resetBoard();
+  }, 1500);
+      }
+
+
+ 
+      }
   }
 
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+cards.forEach(card => card.addEventListener('click', turnCard));
